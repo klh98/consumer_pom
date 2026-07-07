94443667c058c84b25a3e3ba6423d945d283dbfd
Kyaung Lan Htan <kyaunglanhtan@abank.com.mm>
2026-07-07 13:31:02 +0630

    add new code in grab driver biker pin

--- src/test/java/Tests/GrabDriverBikerPinTest.java
+++ src/test/java/Tests/GrabDriverBikerPinTest.java
@@ -1,4 +1,29 @@
 package Tests;
 
-public class GrabDriverBikerPinTest {
+
+import Base.BaseTest;
+import Pages.EAPlayProPage;
+import Pages.GrabDriverBikerPinPage;
+import Pages.MobileLegendPage;
+import Pages.ViberPlusPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GrabDriverBikerPinTest extends BaseTest {
+
+    @Test(priority = 1)
+    public void GrabDriverBikerPin()
+    {
+        GrabDriverBikerPinPage GrabDriverBikerPin = new GrabDriverBikerPinPage(getDriver());
+        GrabDriverBikerPin.openBills();
+        GrabDriverBikerPin.enterSearchText("Grab Driver Biker");
+        GrabDriverBikerPin.click_GrabDriverBikerPin();
+        GrabDriverBikerPin.selectProduct();
+        GrabDriverBikerPin.clickNext();
+        GrabDriverBikerPin.clickSelect();
+        GrabDriverBikerPin.clickBuyNow();
+        GrabDriverBikerPin.enterPin("107198");   //put your password
+        GrabDriverBikerPin.clickBackBtn();
+    }
+
 }
